cbd664dbc3535b5cc5305e62134f95dbefccf6fa
aeking9090 <92158755+aeking9090@users.noreply.github.com>
2026-08-11 08:49:39 +0700

    Add files via upload

--- store-config.js
+++ store-config.js
@@ -3,7 +3,8 @@ window.NovaCartStore = {
   phone: "+84 961xxx-xxx",
   address: "Đ. Số 3, Trung Mỹ Tây, Hồ Chí Minh",
   addressMapQuery: "Đ. Số 3, Trung Mỹ Tây, Hồ Chí Minh",
-  mapEmbedUrl: "https://www.openstreetmap.org/export/embed.html?bbox=106.6627,10.8062,106.7062,10.8730&layer=mapnik&marker=10.8530,106.6820"
+  mapEmbedUrl: "https://www.openstreetmap.org/export/embed.html?bbox=106.6627,10.8062,106.7062,10.8730&layer=mapnik&marker=10.8530,106.6820",
+  googleMapsSearch: "https://www.google.com/maps/search/?api=1&query=%C4%90.%20S%E1%BB%91%203%2C%20Trung%20M%E1%BB%B9%20T%C3%A2y%2C%20H%E1%BB%93%20Ch%C3%AD%20Minh"
 };
 
 (function () {
@@ -19,7 +20,7 @@ window.NovaCartStore = {
     });
 
     phoneEls.forEach((el) => {
-      if (el) el.textContent = store.phone || "+84 xxx-xxxx";
+      if (el) el.textContent = store.phone || "+84 961xxx-xxx";
     });
 
     addressEls.forEach((el) => {
@@ -29,6 +30,11 @@ window.NovaCartStore = {
     const mapFrame = document.getElementById("storeMapFrame");
     if (mapFrame) {
       mapFrame.src = store.mapEmbedUrl || "https://www.openstreetmap.org/export/embed.html?bbox=106.6627,10.8062,106.7062,10.8730&layer=mapnik&marker=10.8530,106.6820";
+    }
+
+    const mapLink = document.getElementById("openGoogleMapLink");
+    if (mapLink) {
+      mapLink.href = store.googleMapsSearch || "https://www.google.com/maps/search/?api=1&query=%C4%90.%20S%E1%BB%91%203%2C%20Trung%20M%E1%BB%B9%20T%C3%A2y%2C%20H%E1%BB%93%20Ch%C3%AD%20Minh";
     }
   }
 
