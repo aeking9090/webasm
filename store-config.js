df16beb791788791beb4fc5822ec5a24466b6741
aeking9090 <92158755+aeking9090@users.noreply.github.com>
2026-08-11 08:31:05 +0700

    Add files via upload

--- store-config.js
+++ store-config.js
@@ -1,6 +1,6 @@
 window.NovaCartStore = {
   email: "wjbu007@gmail.com",
-  phone: "+84 xxx-xxxx",
+  phone: "+84 961xxx-xxx",
   address: "Đ. Số 3, Trung Mỹ Tây, Hồ Chí Minh",
   addressMapQuery: "Đ. Số 3, Trung Mỹ Tây, Hồ Chí Minh",
   mapEmbedUrl: "https://www.openstreetmap.org/export/embed.html?bbox=106.6627,10.8062,106.7062,10.8730&layer=mapnik&marker=10.8530,106.6820"
